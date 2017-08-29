db21568c84ebc6b84fcd7293bbefff46055c5a9a
Jack Westbrook <jacksonwestbrook3@gmail.com>
2017-08-29 16:15:24 -0700

    wired up messages component

--- src/components/MessagesComponent.js
+++ src/components/MessagesComponent.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import MessageComponent from './MessageComponent';
 
-export default function MessagesComponent({ messages, selectedMessageIds }) {
+export default function MessagesComponent({
+  messages,
+  selectedMessageIds,
+  onMarkAsReadMessage,
+  onStarMessage,
+  onUnstarMessage,
+  onSelectMessage,
+  onDeselectMessage
+}) {
   if (!messages) return <h4>"No Messages"</h4>;
 
   return (
     <div>
-      {messages.map(message => <MessageComponent message={message} selected={selectedMessageIds} key={message.id} />)}
+      {messages.map(message =>
+        <MessageComponent
+          message={message}
+          key={message.id}
+          selected={selectedMessageIds.includes(message.id)}
+          onMarkAsReadMessage={onMarkAsReadMessage}
+          onStarMessage={onStarMessage}
+          onUnstarMessage={onUnstarMessage}
+          onSelectMessage={onSelectMessage}
+          onDeselectMessage={onDeselectMessage}
+          selectedMessageIds={selectedMessageIds}
+        />
+      )}
     </div>
   );
 }
